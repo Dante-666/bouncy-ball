67a4bf73821b2f192744f7681673b536770c2d81
Dante-666 <j.singh.logan@gmail.com>
2020-11-01 14:25:17 +0530

    Fixed #55, added Iterator for Chains

--- include/behavior/BChain.h
+++ include/behavior/BChain.h
@@ -17,14 +17,16 @@
  * How is it implemented? By having a linked list which applies the behavior, if
  * enabled, to the entity
  */
-#include "G3D-base/G3DString.h"
-#include "G3D-base/debugAssert.h"
+
+#include "Iterator.h"
 
 namespace G3D {
 
 template <typename E> class BehaviorChain {
-    BehaviorChain *next = nullptr; // 1. "next" pointer in the base class
+    BehaviorChain *next = nullptr;
+
 public:
+    friend class Iterator<BehaviorChain>;
     BehaviorChain() = default;
     // Very simple implementation of BehaviorChain class.
     // TODO: add management here where we can add/remove behaviors
@@ -38,97 +40,19 @@ public:
 
     const virtual String getName() const { return "Base"; }
 
-    // TODO: Make this to Entity
     virtual void apply(E *pEntity) {
         if (next)
             next->apply(pEntity);
     }
 
-    class Iterator {
-        BehaviorChain *node;
+    virtual void makeGUI(class GuiPane *pane, class GApp *app) {}
 
-    public:
-        Iterator(BehaviorChain *node = nullptr) { this->node = node; }
-        bool isLast() { return node->next == nullptr; }
-        bool hasNext() { return node->next; }
-        void advance() {
-            if (hasNext()) {
-                node = node->next;
-            }
-        }
-
-        inline bool operator!=(const Iterator &other) const {
-            return !(*this == other);
-        }
-
-        BehaviorChain *operator->() const {
-            debugAssert(isValidHeapPointer(node));
-            return node;
-        }
-
-        BehaviorChain *operator*() const {
-            debugAssert(isValidHeapPointer(node));
-            return node;
-        }
-    };
-
-    Iterator begin() const { return Iterator(next); }
-    const Iterator end() const { return Iterator(); }
+    Iterator<BehaviorChain> begin() const { return Iterator(next); }
+    const Iterator<BehaviorChain> end() const {
+        return Iterator<BehaviorChain>();
+    }
 
     // TODO: removeBehavior
     void clear() {}
 };
 } // namespace G3D
-
-/*        inline bool operator!=(const Iterator &other) const {
-            return !(*this == other);
-        }
-
-        bool operator==(const Iterator &other) const {
-            if (other.isDone || isDone) {
-                // Common case; check against isDone.
-                return (isDone == other.isDone);
-            } else {
-                return (node == other.node) && (index == other.index);
-            }
-        }
-
-        Iterator &operator++() {
-            debugAssert(!isDone);
-            debugAssert(node != nullptr);
-            debugAssert(isValidHeapPointer(node));
-            debugAssert((node->next == nullptr) ||
-                        isValidHeapPointer(node->next));
-            node = node->next;
-            findNext();
-            debugAssert(isDone || isValidHeapPointer(node));
-            return *this;
-        }
-
-        Iterator operator++(int) {
-            Iterator old = *this;
-            ++(*this);
-            return old;
-        }
-
-        const BehaviorChain &operator*() const { return node; }
-
-        const String &value() const { return node->getName(); }
-
-        // const Key &key() const { return node->entry.key; }
-
-        BehaviorChain *operator->() const {
-            debugAssert(isValidHeapPointer(node));
-            return &(node);
-        }
-
-        operator BehaviorChain *() const {
-            debugAssert(isValidHeapPointer(node));
-            return &(node->entry);
-        }
-
-        bool isValid() const { return !isDone; }
-    };
-
-    Iterator begin() const { return Iterator(m_numBuckets, m_bucket); }
-    */
--- include/behavior/PChain.h
+++ include/behavior/PChain.h
@@ -18,9 +18,7 @@
  * enabled, to the entity
  */
 
-#include "G3D-base/G3DString.h"
-#include "G3D-base/debug.h"
-#include "map"
+#include "Iterator.h"
 
 namespace G3D {
 
@@ -30,6 +28,7 @@ class PropertyChain {
     PropertyChain *next = nullptr;
 
 public:
+    friend class Iterator<PropertyChain>;
     PropertyChain() = default;
     // void setNextC(PropertyChain *n) { next = n; }
     // Very simple implementation of PropertyChain class.
@@ -42,71 +41,13 @@ public:
     }
 
     const virtual String getName() const { return "Base"; }
-    // Property always may have to be constructed and has to do with creation
-    // deletion
-    // TODO: Make this to Entity
-    // TODO: Deprecate
-    const PropertyChain *getNext() const { return next; }
 
-    virtual void makeGUI(class GuiPane* pane, class GApp* app) {}
+    virtual void makeGUI(class GuiPane *pane, class GApp *app) {}
 
-    class Iterator {
-        PropertyChain *node;
-        std::map<String, PropertyChain *> _m_intern;
-        bool isDone = false;
-        bool _map_empty = true;
-
-        void _m_intern_fill() {
-            PropertyChain *first = node;
-            while (!isDone) {
-                _m_intern[node->getName()] = node;
-                advance();
-            }
-	    node = first;
-        }
-
-    public:
-        Iterator(PropertyChain *node = nullptr) { this->node = node; }
-        bool isOver() { return isDone; }
-        bool hasNext() { return node->next != nullptr; }
-        void advance() {
-            if (hasNext()) {
-                node = node->next;
-            } else {
-                isDone = true;
-            }
-        }
-
-        PropertyChain *getByName(const String name) {
-            if (_map_empty) {
-                _m_intern_fill();
-                _map_empty = false;
-            }
-            auto it = _m_intern.find(name);
-            if (it != _m_intern.end()) {
-		return _m_intern[name];
-	    } else {
-		return nullptr;
-	    }
-        }
-
-        /*inline bool operator!=(const Iterator &other) const {
-            return !(*this == other);
-        }*/
-
-        PropertyChain *operator->() const {
-            debugAssert(isValidHeapPointer(node));
-            return node;
-        }
-
-        PropertyChain *operator*() const {
-            debugAssert(isValidHeapPointer(node));
-            return node;
-        }
-    };
-
-    Iterator begin() const { return Iterator(next); }
-    const Iterator end() const { return Iterator(); }
+    Iterator<PropertyChain> begin() const { return Iterator(next); }
+    const Iterator<PropertyChain> end() const {
+        return Iterator<PropertyChain>();
+    }
 
     // TODO: removeBehavior
     void clear() {}
